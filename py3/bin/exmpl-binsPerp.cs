774c95327fdfe65fc7965bba28c4f3dbead78263
Mohsen Banan <form@mohsen.banan.1.byname.net>
2024-09-16 21:12:40 -0700

    Starting point design is in place.

--- py3/bin/exmpl-binsPerp.cs
+++ py3/bin/exmpl-binsPerp.cs
@@ -3,6 +3,32 @@
 
 from bisos.binsprep import interface
 
+"""
+This is the model that we are targetting:
+
+---------
+#!/usr/bin/env python
+
+# dblock for name of the file comes here.
+
+from bisos.binsprep import binsprep
+
+ap = binsprep.aptPkg
+
+aptPkgsList = [
+    ap("djbdns"),
+    ap("", instFn=someFunc),
+]
+
+binsprep.setup(
+    aptPkgsList=aptPkgsList,
+)
+
+# dblock for the exec comes here.
+------------
+"""
+
+
 interface.setFunc("FROM MINI")
 
 __file__ = '/bisos/git/bxRepos/bisos-pip/binsprep/py3/bin/seedBinsPrep.cs'
