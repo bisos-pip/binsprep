06f35daed055c6b842ba4a31298bad19de47c5c6
Mohsen Banan <form@mohsen.banan.1.byname.net>
2024-09-22 23:02:18 -0700

    Proof of concept.

--- py3/bin/exmpl-binsPerp.cs
+++ py3/bin/exmpl-binsPerp.cs
@@ -1,35 +1,19 @@
-#!/bisos/venv/py3/dev-bisos3/bin/python
-
-
-from bisos.binsprep import interface
-
-"""
-This is the model that we are targetting:
-
----------
 #!/usr/bin/env python
 
 # dblock for name of the file comes here.
 
 from bisos.binsprep import binsprep
-
 ap = binsprep.aptPkg
 
 aptPkgsList = [
     ap("djbdns"),
-    ap("", instFn=someFunc),
+    ap("facter"),
+    # ap("", instFn=someFunc),
 ]
 
 binsprep.setup(
     aptPkgsList=aptPkgsList,
 )
-
-# dblock for the exec comes here.
-------------
-"""
-
-
-interface.setFunc("FROM MINI")
 
 __file__ = '/bisos/git/bxRepos/bisos-pip/binsprep/py3/bin/seedBinsPrep.cs'
 with open(__file__) as f:
--- py3/bin/seedBinsPrep.cs
+++ py3/bin/seedBinsPrep.cs
@@ -10,7 +10,7 @@
 * [[elisp:(org-cycle)][| /Control Parameters Of This File/ |]] :: dblk ctrls classifications=cs-u
 #+BEGIN_SRC emacs-lisp
 (setq-local b:dblockControls t) ; (setq-local b:dblockControls nil)
-(put 'b:dblockControls 'py3:cs:Classification "cs-u") ; one of cs-mu, cs-u, cs-lib, b-lib, pyLibPure
+(put 'b:dblockControls 'py3:cs:Classification "cs-u") ; one of cs-mu, cs-u, cs-lib, bpf-lib, pyLibPure
 #+END_SRC
 #+RESULTS:
 : cs-u
@@ -29,7 +29,7 @@
 ####+BEGIN: b:prog:file/particulars :authors ("./inserts/authors-mb.org")
 """ #+begin_org
 * *[[elisp:(org-cycle)][| Particulars |]]* :: Authors, version
-** This File: /bisos/git/auth/bxRepos/bisos-pip/marmee/py3/bin/pkgMarmeManage.cs
+** This File: /bisos/git/bxRepos/bisos-pip/binsprep/py3/bin/seedBinsPrep.cs
 ** Authors: Mohsen BANAN, http://mohsen.banan.1.byname.net/contact
 #+end_org """
 ####+END:
@@ -39,10 +39,10 @@
 * *[[elisp:(org-cycle)][| Particulars-csInfo |]]*
 #+end_org """
 import typing
-csInfo: typing.Dict[str, typing.Any] = { 'moduleName': ['pkgMarmeManage'], }
-csInfo['version'] = '202209284907'
+csInfo: typing.Dict[str, typing.Any] = { 'moduleName': ['seedBinsPrep'], }
+csInfo['version'] = '202409222227'
 csInfo['status']  = 'inUse'
-csInfo['panel'] = 'pkgMarmeManage-Panel.org'
+csInfo['panel'] = 'seedBinsPrep-Panel.org'
 csInfo['groupingType'] = 'IcmGroupingType-pkged'
 csInfo['cmndParts'] = 'IcmCmndParts[common] IcmCmndParts[param]'
 ####+END:
@@ -58,8 +58,9 @@ Module description comes here.
 
 ####+BEGIN: b:prog:file/orgTopControls :outLevel 1
 """ #+begin_org
-* [[elisp:(org-cycle)][| Controls |]] :: [[elisp:(delete-other-windows)][(1)]] | [[elisp:(show-all)][Show-All]]  [[elisp:(org-shifttab)][Overview]]  [[elisp:(progn (org-shifttab) (org-content))][Content]] | [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] | [[elisp:(bx:org:run-me)][Run]] | [[elisp:(bx:org:run-me-eml)][RunEml]] | [[elisp:(progn (save-buffer) (kill-buffer))][S&Q]]  [[elisp:(save-buffer)][Save]]  [[elisp:(kill-buffer)][Quit]] [[elisp:(org-cycle)][| ]]
+* [[elisp:(org-cycle)][| Controls |]] :: [[elisp:(delete-other-windows)][(1)]] | [[elisp:(show-all)][Show-All]]  [[elisp:(org-shifttab)][Overview]]  [[elisp:(progn (org-shifttab) (org-content))][Content]] | [[file:Panel.org][Panel]] | [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] | [[elisp:(bx:org:run-me)][Run]] | [[elisp:(bx:org:run-me-eml)][RunEml]] | [[elisp:(progn (save-buffer) (kill-buffer))][S&Q]]  [[elisp:(save-buffer)][Save]]  [[elisp:(kill-buffer)][Quit]] [[elisp:(org-cycle)][| ]]
 ** /Version Control/ ::  [[elisp:(call-interactively (quote cvs-update))][cvs-update]]  [[elisp:(vc-update)][vc-update]] | [[elisp:(bx:org:agenda:this-file-otherWin)][Agenda-List]]  [[elisp:(bx:org:todo:this-file-otherWin)][ToDo-List]]
+
 #+end_org """
 ####+END:
 
@@ -71,24 +72,15 @@ Module description comes here.
 
 ####+BEGIN: b:py3:cs:framework/imports :basedOn "classification"
 """ #+begin_org
-** Imports Based On Classification=cs-u
+*  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  CsFrmWrk   [[elisp:(outline-show-subtree+toggle)][||]] *Imports* =Based on Classification=cs-u=
 #+end_org """
 from bisos import b
 from bisos.b import cs
 from bisos.b import b_io
+from bisos.common import csParam
 
 import collections
 ####+END:
-
-from  bisos.platform import bxPlatformThis
-from  bisos.platform import bxPlatformConfig
-
-from unisos.common import icmsPkgLib
-from bisos.marmee import marmeePkgThis
-
-import os
-
-from bisos.binsprep import interface
 
 """ #+begin_org
 *  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  CsFrmWrk   [[elisp:(outline-show-subtree+toggle)][||]] ~csuList emacs-list Specifications~  [[elisp:(blee:org:code-block/above-run)][ /Eval Below/ ]] [[elisp:(org-cycle)][| ]]
@@ -97,22 +89,24 @@ from bisos.binsprep import interface
   (list
    "bisos.b.cs.ro"
    "blee.icmPlayer.bleep"
+   "bisos.binsprep.binsprep_csu"
  ))
 #+END_SRC
 #+RESULTS:
-| bisos.b.cs.ro | blee.icmPlayer.bleep |
+| bisos.b.cs.ro | blee.icmPlayer.bleep | bisos.binsprep.binsprep_csu |
 #+end_org """
 
 ####+BEGIN: b:py3:cs:framework/csuListProc :pyImports t :csuImports t :csuParams t
 """ #+begin_org
-*  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  CsFrmWrk   [[elisp:(outline-show-subtree+toggle)][||]] =Process CSU List= with /2/ in csuList pyImports=t csuImports=t csuParams=t
+*  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  CsFrmWrk   [[elisp:(outline-show-subtree+toggle)][||]] =Process CSU List= with /3/ in csuList pyImports=t csuImports=t csuParams=t
 #+end_org """
 
 from bisos.b.cs import ro
 from blee.icmPlayer import bleep
+from bisos.binsprep import binsprep_csu
 
 
-csuList = [ 'bisos.b.cs.ro', 'blee.icmPlayer.bleep', ]
+csuList = [ 'bisos.b.cs.ro', 'blee.icmPlayer.bleep', 'bisos.binsprep.binsprep_csu', ]
 
 g_importedCmndsModules = cs.csuList_importedModules(csuList)
 
@@ -125,31 +119,11 @@ def g_extraParams():
 
 ####+BEGIN: b:py3:cs:main/exposedSymbols :classes ()
 """ #+begin_org
-*  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  CsFrmWrk   [[elisp:(outline-show-subtree+toggle)][||]] ~Exposed Symbols List Specification~ with /0/ in Classes List
+*  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  CsFrmWrk   [[elisp:(outline-show-subtree+toggle)][||]] ~CS Controls and Exposed Symbols List Specification~ with /0/ in Classes List
 #+end_org """
 ####+END:
 
-
-####+BEGIN: bx:cs:python:func :funcName "canon_pythonPkgsSpec" :funcType "anyOrNone" :retType "bool" :deco "" :argsList ""
-""" #+begin_org
-*  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  F-anyOrNone [[elisp:(outline-show-subtree+toggle)][||]] /canon_pythonPkgsSpec/ retType=bool argsList=nil  [[elisp:(org-cycle)][| ]]
-#+end_org """
-def canon_pythonPkgsSpec():
-####+END:
-    pkgs = collections.OrderedDict()
-    pkgs["notmuch"] = None
-    pkgs["flufl.bounce"] = "2.3"
-    return pkgs
-
-####+BEGIN: bx:cs:python:func :funcName "canon_linuxPkgsSpec" :funcType "anyOrNone" :retType "bool" :deco "" :argsList ""
-""" #+begin_org
-*  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  F-anyOrNone [[elisp:(outline-show-subtree+toggle)][||]] /canon_linuxPkgsSpec/ retType=bool argsList=nil  [[elisp:(org-cycle)][| ]]
-#+end_org """
-def canon_linuxPkgsSpec():
-####+END:
-    pkgs = collections.OrderedDict()
-    pkgs["offlineimap"] = None
-    return pkgs
+cs.invOutcomeReportControl(cmnd=True, ro=True)
 
 
 ####+BEGIN: blee:bxPanel:foldingSection :outLevel 0 :sep nil :title "CmndSvcs" :anchor ""  :extraInfo "Command Services Section"
@@ -158,7 +132,7 @@ def canon_linuxPkgsSpec():
 #+end_org """
 ####+END:
 
-####+BEGIN: b:py3:cs:cmnd/classHead :cmndName "examples" :extent "verify" :ro "noCli" :comment "FrameWrk: CS-Main-Examples" :parsMand "" :parsOpt "" :argsMin 0 :argsMax 0 :pyInv ""
+####+BEGIN: b:py3:cs:cmnd/classHead :cmndName "examples" :extent "verify" :ro "noCli" :comment "FrameWrk: CS-Main-Examples" :parsMand "" :parsOpt "" :argsMin "0" :argsMax "0" :asFunc "" :interactiveP ""
 """ #+begin_org
 *  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  CmndSvc-   [[elisp:(outline-show-subtree+toggle)][||]] <<examples>>  *FrameWrk: CS-Main-Examples*  =verify= argsMin=0 argsMax=0 ro=noCli   [[elisp:(org-cycle)][| ]]
 #+end_org """
@@ -175,23 +149,18 @@ class examples(cs.Cmnd):
              argsList: typing.Optional[list[str]]=None,  # CsArgs
     ) -> b.op.Outcome:
         """FrameWrk: CS-Main-Examples"""
+        failed = b_io.eh.badOutcome
         callParamsDict = {}
         if self.invocationValidate(rtInv, cmndOutcome, callParamsDict, argsList).isProblematic():
-            return b_io.eh.badOutcome(cmndOutcome)
+            return failed(cmndOutcome)
         cmndArgsSpecDict = self.cmndArgsSpec()
 ####+END:
         self.cmndDocStr(f""" #+begin_org ***** [[elisp:(org-cycle)][| *CmndDesc:* | ]]  Conventional top level example.
         #+end_org """)
 
-        someStr = interface.getFunc()
-        print(f"commonFunc = {someStr}")
-
-        def cpsInit(): return collections.OrderedDict()
-        def menuItem(verbosity): cs.examples.cmndInsert(cmndName, cps, cmndArgs, verbosity=verbosity) # 'little' or 'none'
-        def execLineEx(cmndStr): cs.examples.execInsert(execLine=cmndStr)
-
-        #logControler = b_io.log.Control()
-        #logControler.loggerSetLevel(20)
+        od = collections.OrderedDict
+        cmnd = cs.examples.cmndEnter
+        literal = cs.examples.execInsert
 
         cs.examples.myName(cs.G.icmMyName(), cs.G.icmMyFullName())
 
@@ -199,68 +168,23 @@ class examples(cs.Cmnd):
 
         bleep.examples_icmBasic()
 
+        cs.examples.menuChapter('=Misc=  *Facilities*')
 
-####+BEGIN: bx:cs:python:cmnd:subSection :title "Dev And Testing"
+        binsprep_csu.examples_csu()
 
-####+END:
+        # cs.examples.menuChapter('=ExecLine Example=  *Example Of Echo GPG Install Commands*')
 
-        cs.examples.menuChapter('*General Dev and Testing IIFs*')
+        # literal(f"""echo sudo apt -y install gnupg""")
 
-        cmndName = "unitTest"
-
-        cmndArgs = ""
-        cps = cpsInit() ; # cps['icmsPkgName'] = icmsPkgName
-        cs.examples.cmndInsert(cmndName, cps, cmndArgs, verbosity='little')
-        cs.examples.cmndInsert(cmndName, cps, cmndArgs, verbosity='full')
-
-        cs.examples.menuChapter('*BinsPreps*')
-
-        cmndName="binsPreps" ; cps = cpsInit() ; cmndArgs = ""
-        menuItem(verbosity='none')
-
-        cmndName="binsPrepsCurInfo" ; cps = cpsInit() ; cmndArgs = ""
-        menuItem(verbosity='none')
-
-        cs.examples.menuSection('*Install ICMs Needed Linux Packages*')
-
-        cmndName="canon_linuxPkgInstall" ; cps = cpsInit() ; cmndArgs = ""
-        menuItem(verbosity='none')
-
-        cs.examples.menuSection('*Install ICMs Needed Python Packages*')
-
-        cmndName="canon_pythonPkgInstall" ; cps = cpsInit() ; cmndArgs = ""
-        menuItem(verbosity='none')
+        b.ignore(ro.__doc__,  cmndArgsSpecDict)  # We are not using these modules, but they are auto imported.
 
         return(cmndOutcome)
 
-        # REST IS IGNORED FOR NOW
-
-        #
-        # ICMs PKG Information
-        #
-
-        icmsPkgInfoBaseDir = marmeePkgThis.pkgBase_configDir()
-        icmsPkgModuleBaseDir = marmeePkgThis.pkgBase_baseDir()
-
-        icmsPkgLib.examples_pkgInfoParsFull(
-            icmsPkgNameSpecification(),
-            icmsPkgInfoBaseDir=icmsPkgInfoBaseDir,
-            icmsPkgModuleBaseDir=icmsPkgModuleBaseDir,
-            icmsPkgControlBaseDir=icmsPkgControlBaseDirDefault(),
-            icmsPkgRunBaseDir=icmsPkgRunBaseDirDefault(),
-        )
-
-        b.ignore(ro.__doc__,)  # We are not using these modules, but they are auto imported.
-
-        return(cmndOutcome)
-
-####+BEGIN: bx:cs:py3:section :title "CS-Commands"
+####+BEGIN: bx:icm:py3:section :title "CS-Commands"
 """ #+begin_org
 *  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  /Section/    [[elisp:(outline-show-subtree+toggle)][||]] *CS-Commands*  [[elisp:(org-cycle)][| ]]
 #+end_org """
 ####+END:
-
-
 
 ####+BEGIN: blee:bxPanel:foldingSection :outLevel 0 :sep nil :title "Main" :anchor ""  :extraInfo "Framework Dblock"
 """ #+begin_org
